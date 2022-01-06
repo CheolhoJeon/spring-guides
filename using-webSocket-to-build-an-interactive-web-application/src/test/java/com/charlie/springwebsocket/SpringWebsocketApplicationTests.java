f1588a10b08fd8bbb61200eddd90fd22dcecd564
Cheolho Jeon <cheolho@edu.hanbat.ac.kr>
2022-01-06 17:15:13 +0900

    #5 Using WebSocket to build an interactive web application

---
+++ using-webSocket-to-build-an-interactive-web-application/src/test/java/com/charlie/springwebsocket/SpringWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charlie.springwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
